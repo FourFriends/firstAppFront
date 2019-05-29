9471be6f542d45db7970d13f7685f45ab622ef72
luoxin <iversonluo@tencent.com>
2019-05-29 18:54:36 +0800

    在右侧增加了操作按钮。

--- src/App.vue
+++ src/App.vue
@@ -32,6 +32,17 @@
           <el-table-column prop="bFinish" label="是否完成" width="100" sortable>
               <template scope="scope"> {{ scope.row.bFinish }} </template>
           </el-table-column>
+
+          <el-table-column
+                  fixed="right"
+                  label="操作"
+                  width="100">
+              <template slot-scope="scope">
+                  <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
+                  <el-button type="text" size="small">编辑</el-button>
+              </template>
+          </el-table-column>
+
       </el-table>
     </el-row>
   </div>
@@ -72,7 +83,7 @@ export default {
     return {
       input: '',
       todoItemList: [],
-      date:"2019-05-28"
+      date:new Date()
     }
   },
 
